59fed2c8c73a39eb4b901b343d82e01afedc5373
iamdevonbutler <jpescione@gmail.com>
2017-06-25 09:33:54 -0400

    basic structure in place

--- Readme.md
+++ Readme.md
@@ -1,3 +1,19 @@
 # Mamba
 
-A resources driven way to write microservices on node.
+A CLI that takes ambigious "resources" and creates a sequence of nodejs microservices.
+
+Koa isn't really a framework, it's more of a library.
+
+This is a framework, w/ the benefits of:
+- code reuse
+- common config
+- common utils
+- worker delegation
+- service delegation
+
+
+Commands
+- status
+- watch
+- build
+- deploy
--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,40 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
 
-console.log(111);
+const vorpal = require('vorpal')();
+const path = require('path');
+const {fileExists} = require('../lib/utils');
+const normalizeConfig = require('../lib/normalizeConfig');
+
+const status = require('../lib/commands/status');
+const watch = require('../lib/commands/watch');
+const build = require('../lib/commands/build');
+const deploy = require('../lib/commands/deploy');
+
+// Load config.
+const configPath = path.join(process.cwd(), 'mamba.config.js');
+if (!fileExists(configPath)) {
+  console.log('Add a "./mamba.config.js" file.');
+  return;
+}
+const config = normalizeConfig(require(configPath));
+
+// Init vorpal.
+vorpal
+  .command('status')
+  .action(status);
+
+vorpal
+  .command('watch')
+  .action(watch);
+
+vorpal
+  .command('build')
+  .action(build);
+
+vorpal
+  .command('deploy')
+  .action(deploy);
+
+vorpal
+  .delimiter(config.delimiter)
+  .show();
