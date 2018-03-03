02481a19f97a085f3eed611ba089235d5969d7f1
iamdevonbutler <jpescione@gmail.com>
2018-03-03 18:11:49 -0500

    cli ui progress

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,10 @@ const {fileExists, logArray, cols} = require('../lib/utils');
 const chalk = require('chalk');
 const chokidar = require('chokidar');
 const {EOL} = require('os');
+const keypress = require('keypress');
+const term = require( 'terminal-kit' ).terminal;
+const stripAnsi = require('strip-ansi');
+
 // const objectInterface = require('js-object-interface');
 
 // const loadAppsConfig = require('../lib/load/loadAppsConfig');
@@ -26,8 +30,11 @@ const state = {};
 // Error handling.
 process.on('unhandledRejection', console.error);
 
-// Set encoding for when we read from stdin.
+// Init terminal settings.
 process.stdin.setEncoding('utf8');
+process.stdin.setRawMode(true);
+keypress(process.stdin);
+// keypress.enableMouse(process.stdout);
 
 // Node version check.
 const majorVersion = +process.version.slice(1).split('.')[0];
@@ -43,26 +50,6 @@ if (majorVersion < 9) {
 
 
 
-
-const loadCommands = require('../lib/load/loadCommands');
-// const objectInterface = require('js-object-interface');
-
-var breadcrumbs = [];
-var commandsPath = path.resolve(__dirname, '../lib/commands');
-var commands = loadCommands(commandsPath);
-
-function getHelpText(commands) {
-  var obj = Object.keys(commands).map(key => ([
-    '    ' + commands[key].name,
-    '    ' + commands[key].description,
-  ]));
-  return `${EOL}  Commands:${EOL}${EOL}${cols(obj, 25)}`;
-};
-
-function cr() {
-  process.stdout.write(`${EOL}$brahma${breadcrumbs.length ? '.' : ''}${chalk.dim(breadcrumbs.join('.'))}: `);
-}
-
 // if you just type in a command it prints the help for that command, same for all subcommands.
 // Infinant number of subcommands.
 // command plugins and subcommand extensions need a way to hook into help
@@ -70,15 +57,66 @@ function cr() {
 // autocomplete functionality
 // load commands needs to load project commands too. let user overwrite brahma commands, so they can make their own remote command for instance.
 // how does help work w. subcommands
+// abstract out terminal UI. make functional and pure, no closure.
 
-process.stdin.on('data', function (data) {
-  var command = data.replace(EOL, '');
+const loadCommands = require('../lib/load/loadCommands');
+// const objectInterface = require('js-object-interface');
+
+var breadcrumbs = [];
+var history = [];
+var delimiter = '$brahma: ';
+var cursorXIndex = 0;
+var commandsRootPath = path.resolve(__dirname, '../lib/commands');
+
+function getLineLength() {
+  var length = delimiter.length + buffer.length;
+  return length;
+};
+
+function write(obj, incrementCursor = true) {
+  if (incrementCursor) {
+    cursorXIndex += obj.length;
+  }
+  term(obj);
+};
+
+function addHistory(obj) {
+  history = history
+    .filter(item => item !== obj)
+    .push(obj);
+};
+
+function getHistoryItem(str) {
+  var obj = history.find(item => item.indexOf(str) === 0);
+  return obj;
+};
+
+function getHelpText(commands) {
+  var obj = Object.keys(commands).map(key => ([
+    '    ' + commands[key].name,
+    '    ' + commands[key].description,
+  ]));
+  return `${EOL}  Commands:${EOL}${EOL}${cols(obj, 25)}${EOL}`;
+};
+
+function cr() {
+  write(`$brahma${breadcrumbs.length ? '.' : ''}${chalk.dim(breadcrumbs.join('.'))}: `);
+}
+
+function handleReturn() {
+  var command = buffer;
+  var commandsPath = path.join(commandsRootPath, ...breadcrumbs.reduce((p, c) => {
+    p.push(c);
+    p.push('commands');
+    return p;
+  }, []));
+  var commands = loadCommands(commandsPath);
   var commandNames = Object.keys(commands);
   if (commandNames.indexOf(command) > -1) {
     breadcrumbs.push(command);
     let subcommandsPath = path.join(commandsPath, command, 'commands');
     let subcommands = loadCommands(subcommandsPath);
-    process.stdout.write(getHelpText(subcommands));
+    write(getHelpText(subcommands));
     cr();
   }
   else if (command.split('/').every(item => '..')) {
@@ -86,13 +124,80 @@ process.stdin.on('data', function (data) {
     cr();
   }
   else {
-    process.stdout.write(`Invalid command (${command})`);
     cr();
+  }
+};
+
+var buffer = '';
+process.stdin.on('data', function (data) {
+  data = stripAnsi(data);
+  console.log(999, data);
+  if (data && data !== '\n' && data !== '\r' && data !== '\r\n') { // @todo for some reason 'return' appends a '\r'.
+    buffer += data;
+    write(data);
   }
 });
 
-// process.stdout.write();
-process.stdout.write(getHelpText(commands) + '\n');
+process.stdin.on('keypress', function (ch, key) {
+  if (key && key.ctrl && key.name === 'c') {
+    buffer = '';
+    process.stdin.pause();
+  }
+  else if (key && key.name === 'up') {
+    term.down(1);
+    let item = getHistoryItem(buffer);
+    if (item) {
+      term(item);
+    }
+  }
+  else if (key && key.name === 'down') {
+    // term();
+  }
+  else if (key && key.name === 'left') {
+    if (cursorXIndex > delimiter.length) {
+      cursorXIndex -= 1;
+      term.left(-1);
+    }
+  }
+  else if (key && key.name === 'right') {
+    let length = getLineLength();
+    if (cursorXIndex < length) {
+      term.right(1);
+      cursorXIndex += 1
+    }
+  }
+  else if (key && key.name === 'backspace') {
+    if (cursorXIndex <= delimiter.length) {
+      console.log(2);
+      // term.left(0);
+    }
+    else {
+      console.log(1);
+      let spaces = buffer.length - cursorXIndex - delimiter.length;
+      console.log(cursorXIndex);
+      buffer = buffer.slice(0, -spaces);
+      term.delete(spaces);
+      term.left(-1);
+    }
+  }
+  else if (key && key.name === 'tab') {
+    console.log('tab');
+  }
+  else if (key && key.name === 'return') {
+    handleReturn();
+    write(EOL);
+    cr();
+    buffer = '';
+  }
+});
+
+// process.stdin.on('mousepress', function (info) {
+//   console.log('got "mousepress" event at %d x %d', info.x, info.y);
+// });
+
+
+var commands = loadCommands(commandsRootPath);
+write(getHelpText(commands), false);
 cr();
 
 
