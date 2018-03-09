74c61e7c40292138b94a5ee130b4416db3150358
iamdevonbutler <jpescione@gmail.com>
2018-03-09 10:12:57 -0500

    tab almost complete

--- cache/cli-history.js
+++ cache/cli-history.js
@@ -1,1 +1,1 @@
-module.exports={"":{"index":0,"items":["install","helpers","remote"]},"remote":{"index":0,"items":[".."]},"helpers":{"index":0,"items":[".."]}}
+module.exports={"":{"index":0,"items":["helpers","install","remote"]},"remote":{"index":0,"items":[".."]},"helpers":{"index":0,"items":[".."]}}
